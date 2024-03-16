aff3209fa86ad179a408750c9edc19690542715d
root <IT22049872@my.sliit.lk>
2024-03-16 04:36:27 +0000

    2024-03-16

--- gurugedara_s1/models/connection.php
+++ gurugedara_s1/models/connection.php
@@ -4,7 +4,7 @@ class Connection{
 
 	public function connect(){
 
-		$link = new PDO("mysql:host=127.0.0.1;dbname=gurugedara_db1", "gurugedara", "Guru@2024%db");
+		$link = new PDO("mysql:host=127.0.0.1;dbname=gurugedara_db1", "avcedits", "Avc@2024%db");
 
 		$link -> exec("set names utf8");
 
